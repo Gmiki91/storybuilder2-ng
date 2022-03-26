ef93e2a602d4c5264d31f39df06465fe2c4172a6
Miklos Gabor <gmiki91@gmail.com>
2022-03-26 18:24:23 +0100

    prod

--- src/app/shared/services/note.service.ts
+++ src/app/shared/services/note.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import moment from "moment";
-import { BehaviorSubject, Observable, map } from "rxjs";
+import { BehaviorSubject, map } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Note } from "../models/note";
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'https://8t84fca4l8.execute-api.eu-central-1.amazonaws.com/dev/api'
 };
